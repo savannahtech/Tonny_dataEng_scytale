027f9fd01fa4a68f133cab41d5f5ae5467b76bef
GYASI <GYASI@DESKTOP-K8EF16B>
2024-02-10 09:32:08 +0000

    update ReadMe,

--- README.md
+++ README.md
@@ -14,6 +14,39 @@
 2. Transform the separate files into this table using Spark (Use schema).
 3. Please save the data to a parquet file.
 
+## Running Locally 
+* Clone the repository and change directory into the root.
+* Ensure you have Apache spark running on your system
+* Confirm you have the `.env` file which has the organization name as referenced in the `.env.sample` file. 
+* Create a python virtual environment using depending on the system you are on. Below is the command for Mac or Linux systems using the virtualenvewrapper, which will create and activate the env. 
+```sh
+    mkvirtualenv myenv
+```
+* Otherwise create and activate with teh virtualenv library 
+```sh
+    virtualenv myenv
+    source myenv/bin/activate
+```
+* When the env is ready, install requirements using
+```sh
+    pip install -r requirements.txt
+```
+* After that, run the `app/main.py` with the command 
+```sh
+    python app/main.py
+```
+* The execution will produce 2 directories 
+
+    * Scytale-exercise
+        
+        * prs
+        * repositories
+    
+    * result
+* The `result` directory contains the parquet output from the execution
+* The `Scytale-exercise` directory contains the raw data from the github organization `prs/{repo_name}_pull_requests.json`, which is all the pull requests from a repository with name placeholder `{repo_name}` and `repositories/data.json` which the full data for all repositories in the organization.
+
+
 ## Running the application
 * Clone the repository.
 * [Optional] If you wish to create an AWS EC2 instance to run the application on, I have created a terraform script that can create a jupyter notebook with apache spark in the `iac` directory you can use for the infrastructure as code. Once created, you can login with the AWS Session Manager from the AWS System Manager service to have SSH access into the EC2 instance. You can thereafter clone the repository on to the EC2 instance. 
--- app/main.py
+++ app/main.py
@@ -9,28 +9,27 @@ import requests
 from transform import transformation_with_spark
 
 
-def get_all_repositories(org_name: str, access_token: str):
+def get_all_repositories(org_name):
     """Retrieve all repositories given the organization name"""
     url = f"https://api.github.com/orgs/{org_name}/repos"
-    headers = {"Authorization": f"Bearer {access_token}"}
     repositories = []
 
     while url:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         repositories.extend(response.json())
 
         # Check for rate limit exceeded
-        if response.status_code == 403 and 'X-RateLimit-Remaining' in response.headers:
-            remaining_limit = int(response.headers['X-RateLimit-Remaining'])
+        if response.status_code == 403 and "X-RateLimit-Remaining" in response.headers:
+            remaining_limit = int(response.headers["X-RateLimit-Remaining"])
             if remaining_limit == 0:
-                reset_time = int(response.headers['X-RateLimit-Reset'])
+                reset_time = int(response.headers["X-RateLimit-Reset"])
                 wait_time = reset_time - int(time.time()) + 5
                 print(f"Rate limit exceeded. Waiting for {wait_time} seconds.")
                 time.sleep(wait_time)
                 continue
 
         # Check for pagination
-        links = response.headers.get('Link')
+        links = response.headers.get("Link")
         if links:
             next_url = get_next_page(links)
             url = next_url
@@ -42,35 +41,34 @@ def get_all_repositories(org_name: str, access_token: str):
 
 def get_next_page(links):
     """Extract the next page URL from the Link header"""
-    link_items = links.split(',')
+    link_items = links.split(",")
     for link_item in link_items:
-        link, rel = link_item.split(';')
+        link, rel = link_item.split(";")
         if 'rel="next"' in rel:
-            return link.strip('<>')
+            return link.strip("<>")
 
 
-def get_all_pull_requests(repo_full_name: str, access_token: str):
+def get_all_pull_requests(repo_full_name: str):
     """Retrieve all pull requests given the repository's name"""
     url = f"https://api.github.com/repos/{repo_full_name}/pulls?state=all"
-    headers = {"Authorization": f"Bearer {access_token}"}
     pull_requests = []
 
     while url:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         pull_requests.extend(response.json())
 
         # Check for rate limit exceeded
-        if response.status_code == 403 and 'X-RateLimit-Remaining' in response.headers:
-            remaining_limit = int(response.headers['X-RateLimit-Remaining'])
+        if response.status_code == 403 and "X-RateLimit-Remaining" in response.headers:
+            remaining_limit = int(response.headers["X-RateLimit-Remaining"])
             if remaining_limit == 0:
-                reset_time = int(response.headers['X-RateLimit-Reset'])
+                reset_time = int(response.headers["X-RateLimit-Reset"])
                 wait_time = reset_time - int(time.time()) + 5
                 print(f"Rate limit exceeded. Waiting for {wait_time} seconds.")
                 time.sleep(wait_time)
                 continue
 
         # Check for pagination
-        links = response.headers.get('Link')
+        links = response.headers.get("Link")
         if links:
             next_url = get_next_page(links)
             url = next_url
@@ -83,26 +81,22 @@ def get_all_pull_requests(repo_full_name: str, access_token: str):
 def save_data_to_json(data, filename):
     """Checks to see a given prefix directory exists or create and save data"""
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
-def main(organization: str, github_token: str):
+def main(organization: str):
     """The main entry point to the extracting"""
 
-    repositories = get_all_repositories(organization, github_token)
+    repositories = get_all_repositories(organization)
 
     if not os.path.exists(organization):
         os.makedirs(organization)
-        save_data_to_json(
-            repositories, f"{organization}/repositories/data.json"
-            )
+        save_data_to_json(repositories, f"{organization}/repositories/data.json")
 
     for repo in repositories:
-        repo_full_name = repo['full_name']
-        pull_requests = get_all_pull_requests(
-            repo_full_name, github_access_token
-            )
+        repo_full_name = repo["full_name"]
+        pull_requests = get_all_pull_requests(repo_full_name)
 
         # Save pull requests data to a JSON file for each repository
         filename = f"{repo_full_name.split('/')[0]}/prs/{repo_full_name.split('/')[1]}_pull_requests.json"
@@ -111,6 +105,5 @@ def main(organization: str, github_token: str):
 
 if __name__ == "__main__":
     organization_name = os.environ.get("ORGANIZATION", "Scytale-exercise")
-    github_access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
-    main(organization_name, github_access_token)
+    main(organization_name)
     transformation_with_spark(organization_name, "result")
